b7545ac0692939d628180bc2e7432a8f781227de
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2016-08-19 16:40:34 -0300

    added the show and delete implementation

--- catalog.py
+++ catalog.py
@@ -48,14 +48,25 @@ def showCategory(category_id):
     #if 'username' not in login_session or creator.id != login_session['user_id']:
      #   return render_template('publicmenu.html', restaurant=restaurant, items=items)
     #else:
-    return render_template('index.html', categories=categories, items=items, category_id = category_id)
+    return render_template('index.html', categories=categories, items=items, category_id=category_id)
 
+
+@app.route('/categories/<int:category_id>/items/<int:item_id>', methods=['GET'])
+def showItem(category_id, item_id):
+    categories = session.query(Category).all()
+
+    #creator = getUserInfo(restaurant.user_id)
+    item = session.query(Item).filter_by(id=item_id).one()
+
+    #if 'username' not in login_session or creator.id != login_session['user_id']:
+     #   return render_template('publicmenu.html', restaurant=restaurant, items=items)
+    #else:
+    return render_template('showItem.html', categories=categories, item=item, category_id=category_id)
 
 @app.route('/categories/<int:category_id>/items/new', methods=['GET', 'POST'])
 def newItem(category_id):
     #if 'username' not in login_session:
      #   return redirect('/login')
-    categories = session.query(Category).all()
 
     if request.method == 'POST':
         newItem = Item(category_id=category_id,
@@ -67,6 +78,42 @@ def newItem(category_id):
         return redirect(url_for('showCategory',category_id = category_id))
     else:
         return render_template('newItem.html', category_id=category_id)
+
+@app.route('/categories/<int:category_id>/items/<int:item_id>/edit', methods=['GET', 'POST'])
+def editItem(category_id,item_id):
+    #if 'username' not in login_session:
+     #   return redirect('/login')
+    edited_item = session.query(Item).filter_by(id=item_id).one()
+
+    if request.method == 'POST':
+
+        if request.form['name']:
+            edited_item.name= request.form['name']
+        if request.form['description']:
+            edited_item.name = request.form['description']
+        session.add(newItem)
+        session.commit()
+
+        #flash("new restaurant edited")
+        return redirect(url_for('showCategory',category_id = category_id))
+    else:
+        return render_template('editItem.html', category_id=category_id, item=edited_item)
+
+@app.route('/categories/<int:category_id>/items/<int:item_id>/delete', methods=['GET', 'POST'])
+def deleteItem(category_id, item_id):
+    #if 'username' not in login_session:
+     #   return redirect('/login')
+    categories = session.query(Category).all()
+    item2Delete = session.query(Item).filter_by(id=item_id).one()
+
+    if request.method == 'POST':
+
+        session.delete(item2Delete)
+        session.commit()
+        #flash("new restaurant created")
+        return redirect(url_for('showCategory',category_id=category_id, categories=categories))
+    else:
+        return render_template('deleteItem.html',category_id=category_id, item=item2Delete)
 
 
 if __name__ == '__main__':
